a2335509a7a00fa84e33f4aa7dfcdffa3d31b94e
CharefSarah <charef.sarah@gmx.fr>
2025-04-20 16:59:43 +0200

    push

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,15 +12,17 @@ menuMobile.innerHTML = `
     <a href="index.html">Accueil</a>
     <a href="about.html">À propos</a>
     <a href="event.html">Événements</a>
-    <a href=contact.html">
-     <button class="btn-primary btn-drawer"><svg
-                        xmlns="http://www.w3.org/2000/svg" width="24"
-                        height="24" viewBox="0 0 24 24" fill="none"
-                        stroke="currentColor" stroke-width="2"
-                        stroke-linecap="round" stroke-linejoin="round"
-                        class="lucide lucide-phone-icon lucide-phone"><path
-                            d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z" /></svg>
-                    Nous contacter</button></a>
+    <a href="contact">Contact</a>
+      <a href="log.html"><button class="btn-primary"><svg
+                                xmlns="http://www.w3.org/2000/svg" width="24"
+                                height="24" viewBox="0 0 24 24" fill="none"
+                                stroke="currentColor" stroke-width="2"
+                                stroke-linecap="round" stroke-linejoin="round"
+                                class="lucide lucide-circle-user-round-icon lucide-circle-user-round"><path
+                                    d="M18 20a6 6 0 0 0-12 0" /><circle cx="12"
+                                    cy="10" r="4" /><circle cx="12" cy="12"
+                                    r="10" /></svg>SE CONNECTER</button></a>
+                </div>
   `;
 document.body.appendChild(menuMobile);
 
